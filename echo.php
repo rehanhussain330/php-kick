861adcd66e4fffa1b906c0edefee444b03a2850d
rehanhussain330 <rehanhussain1996@gmil.com>
2026-04-12 14:11:18 +0100

    done

--- echo.php
+++ echo.php
@@ -1,3 +1,15 @@
 <?php
 
 echo "Hello World";
+echo("hello");
+echo ("hello");
+echo "hello", "world";
+
+print "hello";
+print("hello");
+
+// print "hello","world";
+
+$result = print("hello");
+
+print($result);
